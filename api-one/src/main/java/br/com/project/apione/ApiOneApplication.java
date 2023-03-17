8c273cb7428afc05c278d3879bb201772a14e80a
andre <andre@DESKTOP-8IR2QLS.dhcp-cliente>
2023-03-17 10:50:50 -0300

    Primeiro commit

---
+++ api-one/src/main/java/br/com/project/apione/ApiOneApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.apione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOneApplication.class, args);
+	}
+
+}
---
+++ api-one/src/test/java/br/com/project/apione/ApiOneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.apione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
